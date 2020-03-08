b370dd46f4dae99b6702d62ea6a82c5d9885f2cb
f6ai <fanniszikszai@gmail.com>
2020-03-08 17:22:05 +0100

    added validation for same time / room booking

--- src/app/stepper/stepper.component.ts
+++ src/app/stepper/stepper.component.ts
@@ -6,6 +6,18 @@ import {
   AbstractControl
 } from "@angular/forms";
 
+interface Booking {
+  firstName: string;
+  lastName: string;
+  room: string;
+  date: string;
+  email: string;
+  start: number;
+  end: number;
+  from?: string;
+  to?: string;
+}
+
 @Component({
   selector: "app-stepper",
   templateUrl: "./stepper.component.html",
@@ -14,15 +26,10 @@ import {
 export class StepperComponent implements OnInit {
   rooms: Array<string> = ["Room 1", "Room 2", "Room 3", "Room 4"];
   formGroup: FormGroup;
-  nameFormGroup: FormGroup;
-  emailFormGroup: FormGroup;
-
-  steps = [
-    { label: "Enter your name", content: "Last name, First name." },
-    { label: "Confirm your contact information", content: "123-456-7890" },
-    { label: "Confirm your address", content: "1600 Amphitheater Pkwy MTV" },
-    { label: "You are now done", content: "Finished!" }
-  ];
+  rawFormData;
+  formData: Booking;
+  bookings: Booking[] = [];
+  isBooked: boolean = false;
 
   /** Returns a FormArray with the name 'formArray'. */
   get formArray(): AbstractControl | null {
@@ -47,13 +54,73 @@ export class StepperComponent implements OnInit {
           lastName: ["", Validators.required]
         }),
         this._formBuilder.group({
-          email: ["", Validators.email]
+          email: ["", [Validators.email, Validators.required]]
         })
       ])
     });
   }
 
   onSubmit() {
-    console.log("Submitted information:", this.formGroup);
+    const formArr = this.formGroup.value.formArray;
+    this.rawFormData = formArr.reduce(function(acc, currentObj) {
+      for (var key in currentObj) acc[key] = currentObj[key];
+      return acc;
+    }, {});
+    const day = this.rawFormData.date.getDate();
+    const start = this.combineDateAndTime(
+      this.rawFormData.date,
+      this.rawFormData.from
+    );
+    const end = this.combineDateAndTime(
+      this.rawFormData.date,
+      this.rawFormData.to
+    );
+    this.formData = {
+      firstName: this.rawFormData.firstName,
+      lastName: this.rawFormData.lastName,
+      email: this.rawFormData.email,
+      room: this.rawFormData.selectedRoom,
+      date: this.rawFormData.date,
+      from: this.rawFormData.from,
+      to: this.rawFormData.to,
+      start,
+      end
+    };
+    this.checkIfBooked(this.bookings, this.formData);
+    if (!this.isBooked) {
+      this.bookings.push(this.formData);
+    }
+    console.log(this.bookings);
   }
+
+  checkIfBooked(bookingsArr, newEntry) {
+    if (bookingsArr) {
+      // filter the bookings array by date and room (in case of large obj)
+      const filteredArr = bookingsArr.filter(elem => {
+        return elem.date === newEntry.date && elem.room === newEntry.room;
+      });
+
+      filteredArr.map(item => {
+        if (item.start === newEntry.start || item.end === newEntry.end) {
+          return (this.isBooked = true);
+        } else if (item.start < newEntry.start && item.end < newEntry.end) {
+          return (this.isBooked = false);
+        } else if (item.start > newEntry.start && item.end > newEntry.end) {
+          return (this.isBooked = false);
+        } else {
+          return (this.isBooked = true);
+        }
+      });
+    }
+  }
+
+  private combineDateAndTime = function(date, time) {
+    let timeSec = time + ":00";
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1; // Jan is 0, dec is 11
+    let day = date.getDate();
+    let dateString = "" + year + "-" + month + "-" + day;
+    let combined = new Date(dateString + " " + timeSec).getTime();
+    return combined;
+  };
 }
